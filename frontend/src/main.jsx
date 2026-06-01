e5a239d241e49d74dc24908f215bbd3ffe9d6d05
dngabirano63-code <dngabirano63@gmail.com>
2026-06-01 22:01:20 +0200

    initial commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,15 @@ try { const t = localStorage.getItem("wavechat_theme") || "dark"; document.docum
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import "./styles/tokens.css";
+import "./styles/layout.css";
+import "./styles/messages.css";
+import "./styles/modals.css";
+import "./styles/stories.css";
+import "./styles/calls.css";
+import "./styles/landing.css";
+import "./styles/admin.css";
+import "./styles/responsive.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
